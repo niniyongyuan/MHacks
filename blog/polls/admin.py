d5d3bcf1402fd186cb46708df7706273e50a09dc
niniyongyuan <lilinayongyuan@gmail.com>
2014-01-18 10:29:17 -0500

    a lot to work

---
+++ blog/polls/admin.py
@@ -0,0 +1,24 @@
+from django.contrib import admin
+from polls.models import Poll, Choice
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 2
+
+class PollAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+
+    list_display = ('question', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question']
+
+
+
+
+admin.site.register(Poll, PollAdmin)
+
+#You can assign arbitrary HTML classes to each fieldset
